f0bfef45fdb45fb35c24eebed290b3e1161b052f
Arnold Schultz <dramicos@yahoo.com>
2022-10-12 21:19:42 -0500

    ranges now goes through mongo and plots to index under the map

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@
     
     console.log("hello");
 
-    const rangeData = "data/EV-Ranges.json";
+    const rangeData = "ev-ranges";
 
     function init() {
         
@@ -15,11 +15,61 @@
     init();
      
     // Use plotly maps with JSON data to make a horizontal bar graph of top 10 cultures
-    function rangePlot(yearMin = 2002, yearMax = 2021) {
+    function rangePlot(yearMin = 2010, yearMax = 2021) {
         d3.json(rangeData).then(function(data) {
             // Setting the initial plot parameters
             console.log("rangePlot: ", data);
-            
+
+            // some error proofing -- Add more later
+            if (yearMin < 2010){
+                yearMin = 2010
+            }
+
+
+
+            // Setting the initial plot parameters
+            let ranges = data.data
+
+            // Create indexes for slicing from the years
+            let minYearIndex = yearMin - 2010;
+            let maxYearIndex = yearMax - 2010;
+       
+            let plotSample = ranges.slice(minYearIndex,maxYearIndex+1);
+            console.log("yearsPlot: ", plotSample);
+            let  plotYears = [];
+            let  plotAvgRange = [];
+            let  plotMaxRange = [];
+            for (let i =0; i < plotSample.length; i++){
+                plotYears.push(plotSample[i][0]);
+                plotAvgRange.push(plotSample[i][1]);
+                plotMaxRange.push(plotSample[i][2]);
+            }
+
+            let plot1 = {
+            type : 'bar',
+            name : 'Average Ranges',
+            x: plotYears,
+            y: plotAvgRange,     
+            };
+
+            let plot2 = {
+                type : 'bar',
+                name : 'Max Ranges',
+                x: plotYears,
+                y: plotMaxRange,     
+            };
+
+            let trace = [plot1, plot2];
+
+            let layout = {
+                title : "Average and Maximum Ranges for EVs over the years",
+                margin: {t: 50, l: 140},
+                barmode: 'group'
+            };
+
+            let config = {responsive: true};
+      
+            Plotly.newPlot("bar-plot", trace, layout, config);
 
         });
     }
